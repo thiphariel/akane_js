fe23916a52c77bf467895a352729e48add01f2ea
Thiph <me@thiph.dev>
2023-12-12 14:07:00 +0100

    front: add Century Gothic font

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,10 +2,31 @@ import Header from '@/components/header'
 import ThemeSwitcher from '@/components/theme-switcher'
 import { apiCall } from '@/config'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const century = localFont({
+  src: [
+    {
+      path: '../../public/fonts/CenturyGothicRegular.ttf',
+      weight: '500',
+    },
+    {
+      path: '../../public/fonts/CenturyGothicItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/CenturyGothicBold.ttf',
+      weight: '800',
+    },
+    {
+      path: '../../public/fonts/CenturyGothicBoldItalic.ttf',
+      weight: '800',
+      style: 'italic',
+    },
+  ],
+})
 
 export const metadata: Metadata = {
   title: 'Akane',
@@ -22,7 +43,7 @@ export default async function RootLayout({
   return (
     <html lang="en" className="dark !scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 dark:bg-gray-950 text-gray-950 dark:text-gray-50 dark:text-opacity-90 relative transition-colors h-[5000px]`}>
+        className={`${century.className} bg-gray-50 dark:bg-gray-950 text-gray-950 dark:text-gray-50 dark:text-opacity-90 relative transition-colors h-[5000px]`}>
         <Header menuItems={menuItems} />
         <main className="mt-24 flex justify-center">{children}</main>
         <ThemeSwitcher />
--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -18,7 +18,7 @@ export default function Header({ menuItems }: { menuItems: MenuItems }) {
         animate={{ y: 0, opacity: 1 }}
       />
       <nav className="fixed left-1/2 top-[0.15rem] flex h-[3.75rem] -translate-x-1/2 py-2 sm:top-[1.65rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-sm font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-sm font-bold text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {items?.map(item => (
             <motion.li
               key={item.id}
